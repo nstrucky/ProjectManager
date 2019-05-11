4555a309b8fe6c2898d52dfb75dd3868a65d1e43
nick <nickstruckmeyer@gmail.com>
2019-05-10 22:13:57 -0400

    Fixed filtering bug
    Parentheses were needed around the OR statements in the ProjectDao to separate logically from AND

--- app/src/main/java/com/ventoray/projectmanager/data/dao/ProjectDao.kt
+++ app/src/main/java/com/ventoray/projectmanager/data/dao/ProjectDao.kt
@@ -28,9 +28,9 @@ interface ProjectDao {
     @Query("DELETE FROM projects")
     fun deleteAll(): Int
 
-    @Query("SELECT * FROM projects WHERE projects.name LIKE :query OR projects.account_name LIKE :query AND status <> 'Completed'")
+    @Query("SELECT * FROM projects WHERE (name LIKE :query OR account_name LIKE :query) AND status <> 'Completed'")
     fun searchActiveProjects(query: String): LiveData<List<Project>>
 
-    @Query("SELECT * FROM projects WHERE projects.name LIKE :query OR projects.account_name LIKE :query AND status = 'Completed'")
+    @Query("SELECT * FROM projects WHERE (name LIKE :query OR account_name LIKE :query) AND status = 'Completed'")
     fun searchCompletedProjects(query: String): LiveData<List<Project>>
 }
